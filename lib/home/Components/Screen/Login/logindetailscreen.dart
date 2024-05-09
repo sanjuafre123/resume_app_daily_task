8cfd613ea86fb9f07d8f0a1d0e9e90e140829c34
pnkaj1999 <pankajgit525@gmail.com>
2024-05-09 13:57:55 +0530

    09/05/2024

--- lib/home/Components/Screen/Login/logindetailscreen.dart
+++ lib/home/Components/Screen/Login/logindetailscreen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,74 +10,122 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController txtEmail = TextEditingController();
+    TextEditingController txtPassword = TextEditingController();
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 60,
-          ),
-          Row(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Image.asset(
-                  'assets/img/bg2.jpg',
-                  height: 220,
-                  fit: BoxFit.cover,
-                ),
+              const SizedBox(
+                height: 60,
               ),
-            ],
-          ),
-          const Column(
-            children: [
               Row(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Welcome Back,',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 38),
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Image.asset(
+                      'assets/img/bg2.jpg',
+                      height: 220,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ],
               ),
-              Row(
+              const Column(
                 children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Make it Work, make it right, make it fast.',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Welcome Back,',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 38),
+                        ),
                       ),
-                    ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Text(
+                          'Make it Work, make it right, make it fast.',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(18),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    prefixIcon: Icon(
-                      Icons.person_outlined,
-                      color: Color(0xffababab),
-                    ),
-                    hintText: 'Sanjuafre08@gmail.com',
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: txtEmail,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                  hintText: 'sanjuafre@gmail.com',
+                  prefixIcon: const Icon(Icons.person_outlined),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(9),
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: txtPassword,
+                obscureText: true,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.fingerprint),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  suffixIcon: const Icon(Icons.remove_red_eye),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10, top: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forget Password?',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Color(0xff272727),
+                      border: Border.all(color: Colors.grey,width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(9))
+                    ),
+                  )
+                ],
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
